ebcc7fc289a071a7cc2ca3818ca0a74ee0c8ca92
brian.carranza <carranza.brian@globalsolutionslt.com>
2025-06-13 09:25:48 -0600

    Init

---
+++ infologic-frontend/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.gsl.app',
+  appName: 'FacturacionGSL',
+  webDir: 'dist/fact-electr-cnr'
+};
+
+export default config;
